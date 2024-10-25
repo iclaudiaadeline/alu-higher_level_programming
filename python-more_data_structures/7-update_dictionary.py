24ae193fd93abcc83d038d888a8c90d9aa41fbac
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-25 21:01:34 +0200

    a function that replaces or adds key/value in a dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+# update_dictionary({'a': 'a', 'b': 'b', 'c': 'c'}, 'a', 'A')
