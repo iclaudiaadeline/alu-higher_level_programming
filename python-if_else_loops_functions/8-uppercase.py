d5c55d64dfcedc772185f7f1becc21dd9c15ba11
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-25 22:52:07 +0200

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
-
-def uppercase(text):
-    charsBag = str(text)
-    res = ""
-    for c in charsBag:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            translation = ord(c) - ord('a')
-            res += chr(ord('A') + translation)
-        else:
-            res += c
-    print("{}".format(res))
+def uppercase(str):
+   result = ""
+   for c in str:
+       if ord(c) >= 97 and ord(c) <= 122:
+           result += chr(ord(c) - 32)
+       else:
+           result += c
+   print("{}".format(result))
