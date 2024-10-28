7941553d01ab31ba0c1310d750788624787f1233
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-28 19:26:11 +0200

    list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhthon3
+def safe_print_list(my_list=[], x=0
+
+  count = 0
+  try:
+  for i in range (x):
+      print(my_list[i], end="")
+      count += 1
+      except IndexError:
+          pass  # ignore the error when ex is greater than the length of my_list
+      print() # print a newline at the end
+      return count
