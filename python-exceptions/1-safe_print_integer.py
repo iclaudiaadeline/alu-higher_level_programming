ff9c6abff55253bd6b650bdc49d7aaf33abe723a
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-29 04:21:55 +0200

    printing of an integers list

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        #attempt to format the value as an intiger
+        # attempt to format the value as an intiger
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
         # If there's an error , return False
         return False
-
