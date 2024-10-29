020f1721a9aee076cbd6ac6ecdbf5ccf0051f2df
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-29 04:02:29 +0200

    list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0
-
-  count = 0
-  try:
-  for i in range (x):
-      print(my_list[i], end="")
-      count += 1
-      except IndexError:
-          pass  # ignore the error when ex is greater than the length of my_list
-      print() # print a newline at the end
-      return cout
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass  # Ignore the error if x is greater than the length of my_list
+    print()  # Print a newline after all elements are printed
+    return count
