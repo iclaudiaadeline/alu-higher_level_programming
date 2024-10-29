d41980777a75e2fcfcbab9f97df225631da429ce
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-29 05:12:43 +0200

    dividiving a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,24 +4,28 @@ def list_division(my_list_1, my_list_2, list_length):
     
     for i in range(list_length):
         try:
-            # Check if the current index is within the bounds of the lists
+            # Initialize the variables for division
             num1 = my_list_1[i]
             num2 = my_list_2[i]
-            
-            # Attempt the division
             result.append(num1 / num2)
         
         except IndexError:
-            # If any list is too short
+            # If either list is too short
             print("out of range")
             result.append(0)
+
         except TypeError:
             # If either element is not an integer or float
             print("wrong type")
             result.append(0)
+
         except ZeroDivisionError:
             # If there is an attempt to divide by zero
             print("division by 0")
             result.append(0)
+
+        finally:
+            # This block can be used for any cleanup or logging if needed
+            pass
     
     return result
