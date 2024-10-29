5d3504f3de6fbf81b0f39818b841d70d85717a35
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-10-29 04:54:09 +0200

    printing and counting

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            # Attempt to print the integer
+            # Attempt to print the integer with "{:d}".format()
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
-            # Skip non_integer elemants silently
+            # Skip non-integer elements silently
             continue
         except IndexError:
-            # Stop if x is larger than list length 
+            # Stop if x is larger than list length
             break
-        print () # Newline after printing
-        return count
+    print()  # Print a newline after the loop
+    return count
