a2d3770aee031a46c10bd7f000e2731b5ef9e9ee
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-11-07 18:05:59 +0200

    python exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    n = 0
     for i in range(x):
         try:
-            # Attempt to print the integer
             print("{:d}".format(my_list[i]), end="")
-            count += 1
+            n += 1
         except (ValueError, TypeError):
-            # Skip non-integer elements silently
             continue
-        except IndexError:
-            # Stop if x is larger than the list length
-            break
-    print()  # Newline after printing
-    return count
+    print()
+    return n
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    result = []
-    
+    new_list = []
     for i in range(list_length):
         try:
-            # Initialize the variables for division
-            num1 = my_list_1[i]
-            num2 = my_list_2[i]
-            result.append(num1 / num2)
-        
-        except IndexError:
-            # If either list is too short
-            print("out of range")
-            result.append(0)
-
-        except TypeError:
-            # If either element is not an integer or float
+            result = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
             print("wrong type")
-            result.append(0)
-
+            result = 0
         except ZeroDivisionError:
-            # If there is an attempt to divide by zero
             print("division by 0")
-            result.append(0)
-
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
         finally:
-            # This block can be used for any cleanup or logging if needed
-            pass
-    
-    return result
+            new_list.append(result)
+    return new_list
