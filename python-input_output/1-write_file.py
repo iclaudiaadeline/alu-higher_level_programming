c1ab21ff9fe4d5527e6c5ea90bd004ed965cc330
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-11-11 14:16:57 +0200

    input and output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """
-This module contains a function that reads a text file and prints it to stdout.
+This module contains a function that reads a text file and prints it .
 """
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """Reads a text file.
+    args:
+         filename: name of the file
+    """
+
+
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
