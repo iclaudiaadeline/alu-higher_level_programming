2c78e5f0db72ac549e327ccf9d79ebee91e1e972
iclaudiaadeline <a.iradukun3@alustudent.com>
2024-11-27 10:49:04 +0200

    python test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains a function that adds 2 integers
+"""
 
-# This module contains a function that adds 2 integers
 
 def add_integer(a, b=98):
-    
-    # Adds two integers or floats (casted to integers).
-
-    if not isinstance(a, (int, float)):
+    """
+    Function that adds 2 integers
+    """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
