80cb1fe5720cad9e11ec9fbea46b453c7d262999
iclaudiaadeline <a.iradukun3@alustudent.com>
2025-01-23 18:30:05 +0200

    javascript

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-
-const x = parseInt(process.argv[2]);
-
-if (isNaN(x)) {
-    console.log('Missing number of occurrences');
-} else if (x > 0) {
-    let i = 0;
-    while (i < x) {
-        console.log('C is fun');
-        i++;
-    }
-}
+const arg = process.argv[2];
+const numberOccurrences = parseInt(arg);
+if (isNaN(numberOccurrences)) console.log('Missing number of occurrences');
+else for (let i = 0; i < numberOccurrences; i++) console.log('C is fun');
