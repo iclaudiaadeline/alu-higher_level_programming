4253fb2647a6874842e0904b5599ed783c269b28
iclaudiaadeline <a.iradukun3@alustudent.com>
2025-01-29 13:18:43 +0200

    javscript

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,14 +2,10 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0){
- console.log('No argument');
-}
-
-else if (args.length === 1){
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
   console.log('Argument found');
-}
-
-else{
- console.log('Arguments found');
+} else {
+  console.log('Arguments found');
 }
